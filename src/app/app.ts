3c0f5b0b3b38eac56ee7b7e14a2fa27cd8dd69f2
Kumaresan Govindan <kumaresang_123@yahoo.com>
2026-05-14 18:45:45 +0530

    udpated webpack.config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe } from "@vcb/shared-libs";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
